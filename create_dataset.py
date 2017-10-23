5a7643240d8d50fe4aad90aa4f30618037117f8c
Samuel Moseley <sammyjmoseley@yahoo.com>
2017-10-23 12:48:10 -0400

    Add data generation

---
+++ create_dataset.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import numpy as np
+import random
+import tensorflow as tf
+
+# amount training images are rotated in file (wrp to x axis)
+prerotation = {1: 90, 2: 0, 3: 45}
+img_size = (28, 28)
+img_shape = img_size + (1,)
+
+src_im = Image.open('data/arrow2.png')
+im = src_im.convert('RGBA')
+
+def generate_data(size):
+    x = []
+    y = []
+    for i in range(0, size):
+        rot_ang = random.randrange(0, 360)
+        dst_im = Image.new("RGBA", img_size, "white")
+        rot = im.rotate(rot_ang, expand=1).resize((28, 28))
+        dst_im.paste(rot, (0, 0), rot)
+        data = np.matmul(np.asarray(dst_im), [1.0 / 3.0, 1.0 / 3.0, 1 / 3.0, 0]).reshape(img_shape)
+        label = (rot_ang + prerotation[2]) % 360
+        x.append(data)
+        y.append(label)
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
+    x = tf.contrib.data.Dataset.from_tensors(tf.constant(x))
+    y = tf.contrib.data.Dataset.from_tensors(tf.constant(y))
+    return tf.contrib.data.Dataset.zip((x, y))
+
+print generate_data(1)
--- tensorflow_starter.py
+++ tensorflow_starter.py
@@ -1,29 +1,37 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+x_data = sess.run(tf.random_uniform([10, 2]))
+m = tf.constant([[1.0], [2.0]])
+y_data = tf.matmul(x_data, m).eval()
+train_x = tf.contrib.data.Dataset.from_tensors(x_data)
+train_y = tf.contrib.data.Dataset.from_tensors(y_data)
+# mnist = tf.contrib.data.Dataset.zip((train_x, train_y))
 
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
+
+
+x = tf.placeholder(tf.float32, shape=[None, 2])
+y_ = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.zeros([2, 1]))
+b = tf.Variable(tf.zeros([1]))
 
 sess.run(tf.global_variables_initializer())
 
 y = tf.matmul(x, W) + b
 
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+cross_entropy = tf.nn.l2_loss(tf.subtract(y_, y))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 
-for _ in range(1000):
-  batch = mnist.train.next_batch(100)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+for _ in range(10000):
+  # batch = mnist.next_batch(100)
+  # train_step.run(feed_dict={x: train_x, y_: train_y})
+  sess.run(train_step, feed_dict={x: x_data, y_: y_data})
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(W.eval())
